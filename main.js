5116174d4b288b3634d0c0afc29eaaf27ebf7c50
Doha-20 <dohaasif22@gmail.com>
2024-03-28 01:45:56 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+//Store a person’s name in a variable, and print a message to that person. Your message should be simple, 
+Object.defineProperty(exports, "__esModule", { value: true });
+let PersonName = "Doha Asif";
+console.log("Hello", PersonName, "would you like to learn some Python today?");
---
+++ main.ts
@@ -0,0 +1,4 @@
+//Store a person’s name in a variable, and print a message to that person. Your message should be simple, 
+
+let PersonName = "Doha Asif";
+console.log("Hello",PersonName, "would you like to learn some Python today?");
